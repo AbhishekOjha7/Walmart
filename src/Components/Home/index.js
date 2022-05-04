1a02764f670a258f0f06d22632c56c8012417fcc
admin <admin@admins-MacBook-Pro-109.local>
2022-05-04 12:22:31 +0530

    third change

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -9,48 +9,9 @@ import { useNavigation } from '@react-navigation/native';
 import Data from '../../Munafa';
 
 
-const _renderItem = ({ item }) => {
-    console.log("hmar list", item)
-    return (
-            <TouchableOpacity
-         
-            activeopacity={0.4}>
-        <View style={styles.parent}>
-               
-                    <Image style={styles.card} source={item.img} />
-      
 
-            <Text style={[styles.cardtext, { fontSize: 8, }]}>{item.title}</Text>
-        </View>
-            </TouchableOpacity>
-    )
-}
 
-const _renderItem1=({item})=>{
-    return(
-        <View style={styles.Discount}>
-        <View>
-            <Image style={styles.DiscountImage} source={require('../../assets/images/20percent.png')} />
-        </View>
-        <View style={styles.parents}>
-            <Image style={styles.cards} source={item.img} />
-            <View>
-                <Text style={styles.cardtexts}>{item.head}</Text>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: windowWidth * 2 / 4.5, marginTop: 20 }}>
-                    <Text style={styles.cardprice}>{item.Price}</Text>
-                    <Text style={styles.carddrop}>{item.pricedrop}</Text>
-                </View>
-                <View style={styles.margin}>
-                    <Text style={styles.margintext}>{item.Margin}</Text>
-                </View>
-            </View>
-        </View>
-        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')} >
-            <Text style={styles.login}>{item.Buy}</Text>
-        </TouchableOpacity>
-    </View>
-    )
-}
+
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -58,6 +19,51 @@ const windowHeight = Dimensions.get('window').height;
 export default function Home() {
 
     const navigation = useNavigation()
+
+    const _renderItem = ({ item }) => {
+        console.log("hmar list", item)
+        return (
+            <TouchableOpacity
+            onPress={()=>navigation.navigate('Offers')}
+                activeopacity={0.8}>
+                  
+                <View style={styles.parent}>
+                    {/* <View style={styles.parent}> */}
+                    <Image style={styles.card} source={item.img} />
+                    {/* </View> */}
+    
+                    <Text style={[styles.cardtext, { fontSize: 8, }]}>{item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+    const _renderItem1 = ({ item }) => {
+        return (
+            <View style={styles.Discount}>
+                <View>
+                    <Image style={styles.DiscountImage} source={require('../../assets/images/20percent.png')} />
+                </View>
+                <View style={styles.parents}>
+                    <Image style={styles.cards} source={item.img} />
+                    <View>
+                        <Text style={styles.cardtexts}>{item.head}</Text>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: windowWidth * 2 / 4.5, marginTop: 20 }}>
+                            <Text style={styles.cardprice}>{item.Price}</Text>
+                            <Text style={styles.carddrop}>{item.pricedrop}</Text>
+                        </View>
+                        <View style={styles.margin}>
+                            <Text style={styles.margintext}>{item.Margin}</Text>
+                        </View>
+                    </View>
+                </View>
+                <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login')} >
+                    <Text style={styles.login}>{item.Buy}</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
     return (
         <SafeAreaView style={styles.inner}>
             <View style={styles.inner1}>
@@ -110,28 +116,28 @@ export default function Home() {
                 </View>
                 <ScrollView showsVerticalScrollIndicator={false}>
                     <TouchableOpacity
-                       onPress={()=>navigation.navigate("Offers")}
-                       activeopacity={0.4}>
-                    <View>
-                        <SliderBox images={[
-                            require('../../assets/images/Sliderbar1.png'),
-                            require('../../assets/images/Slide2.png'),
-                            require('../../assets/images/slide3.png'),
-                            require('../../assets/images/slide4.png'),
-                            require('../../assets/images/slide5.png'),
-                            require('../../assets/images/slide6.png'),
-                            require('../../assets/images/slide7.png'),
-                            require('../../assets/images/slide8.png'),
-                            require('../../assets/images/slide9.png')
-                        ]}
-                            circleLoop
-                            autoplay={true}
-                            sliderBoxHeight={180}
-                            resizeMode='contain'
-                            dotColor='black' />
-                    </View>
+                        onPress={() => navigation.navigate("Offers")}
+                        activeopacity={0.4}>
+                        <View>
+                            <SliderBox images={[
+                                require('../../assets/images/Sliderbar1.png'),
+                                require('../../assets/images/Slide2.png'),
+                                require('../../assets/images/slide3.png'),
+                                require('../../assets/images/slide4.png'),
+                                require('../../assets/images/slide5.png'),
+                                require('../../assets/images/slide6.png'),
+                                require('../../assets/images/slide7.png'),
+                                require('../../assets/images/slide8.png'),
+                                require('../../assets/images/slide9.png')
+                            ]}
+                                circleLoop
+                                autoplay={true}
+                                sliderBoxHeight={180}
+                                resizeMode='contain'
+                                dotColor='black' />
+                        </View>
                     </TouchableOpacity>
-              
+
 
                     <View style={styles.Munafa}>
                         <Text style={styles.munafatext}>{'Munafa Deals'}</Text>
@@ -150,7 +156,7 @@ export default function Home() {
                             </TouchableOpacity>
 
                             <TouchableOpacity
-                             onPress={() => navigation.navigate('Offers')}>
+                                onPress={() => navigation.navigate('Offers')}>
 
 
                                 <View style={styles.parentcard}>
@@ -165,7 +171,7 @@ export default function Home() {
 
                         <View style={{ flexDirection: "row" }}>
                             <TouchableOpacity
-                             onPress={() => navigation.navigate('Offers')}>
+                                onPress={() => navigation.navigate('Offers')}>
 
 
                                 <View style={styles.parentcard}>
@@ -178,7 +184,7 @@ export default function Home() {
                             </TouchableOpacity>
 
                             <TouchableOpacity
-                             onPress={() => navigation.navigate('Offers')}>
+                                onPress={() => navigation.navigate('Offers')}>
 
 
                                 <View style={styles.parentcard}>
@@ -275,41 +281,41 @@ export default function Home() {
                             dotColor='black' />
                     </View>
 
-                    
-                        <View style={styles.zone}>
-                            <Text style={{fontSize:20}}>{'Offer Zone'}</Text>
-                        </View>
-                        
-                        <View style={{flexDirection:'row',justifyContent:"space-between",padding:10}}>
-                            <View style={{}}>
-                            <Image style={{height:windowHeight/7,width:windowWidth/2.15}} source={require('../../assets/images/new.png')}/>
-                            </View>
-                           <View>
-                           <Image style={{height:windowHeight/7,width:windowWidth/2.15}} source={require('../../assets/images/coca.png')}/>
-                           </View>
-                        </View>
 
-                        <View style={{flexDirection:'row',justifyContent:'space-between'}}>
-                             <View style={{padding:5}}>
-                             <Text style={{fontSize:20}}>{"Best Price Exclusive"}</Text>
-                             </View>
-                             <TouchableOpacity
-                             onPress={() => navigation.navigate('Offers')}
-                             >
-                             <View style={{padding:5}}>
-                            <Text style={{fontSize:18,fontWeight:'bold',color:'#047BD5'}}>{'See All'}</Text>
-                            </View>
-                             </TouchableOpacity>
-                         
+                    <View style={styles.zone}>
+                        <Text style={{ fontSize: 20 }}>{'Offer Zone'}</Text>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', justifyContent: "space-between", padding: 10 }}>
+                        <View style={{}}>
+                            <Image style={{ height: windowHeight / 7, width: windowWidth / 2.15 }} source={require('../../assets/images/new.png')} />
                         </View>
-                        <FlatList
+                        <View>
+                            <Image style={{ height: windowHeight / 7, width: windowWidth / 2.15 }} source={require('../../assets/images/coca.png')} />
+                        </View>
+                    </View>
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={{ padding: 5 }}>
+                            <Text style={{ fontSize: 20 }}>{"Best Price Exclusive"}</Text>
+                        </View>
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Offers')}
+                        >
+                            <View style={{ padding: 5 }}>
+                                <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#047BD5' }}>{'See All'}</Text>
+                            </View>
+                        </TouchableOpacity>
+
+                    </View>
+                    <FlatList
                         data={Data}
                         horizontal
                         renderItem={_renderItem1}
                         showsHorizontalScrollIndicator={false}
-                        />
+                    />
 
-                
+
 
                 </ScrollView>
             </View>
@@ -443,8 +449,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
 
     },
-    zone:{
-        padding:15
+    zone: {
+        padding: 15
     },
 
 
@@ -452,9 +458,9 @@ const styles = StyleSheet.create({
         borderColor: 'lightgrey',
         marginTop: 5,
         borderWidth: 1,
-        justifyContent:'space-between', 
-        marginHorizontal:5,
-        borderRadius:5
+        justifyContent: 'space-between',
+        marginHorizontal: 5,
+        borderRadius: 5
     },
     DiscountImage: {
         height: 50,
@@ -466,7 +472,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         height: 150,
-        justifyContent:'space-between'
+        justifyContent: 'space-between'
     },
     cardtext: {
         fontSize: 18,
@@ -498,24 +504,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#047BD5',
         alignItems: 'center',
         padding: 10,
-        marginHorizontal:windowWidth/8,
-    
+        marginHorizontal: windowWidth / 8,
+
         borderRadius: 20,
         marginBottom: 10,
-        
+
     },
     margin: {
         borderWidth: 2,
         marginTop: 10,
         borderColor: '#4E944F',
         borderRadius: 5,
-        marginRight:10
+        marginRight: 10
     },
     margintext: {
         fontSize: 18,
         color: '#4E944F'
     }
-    
+
 
 
 
